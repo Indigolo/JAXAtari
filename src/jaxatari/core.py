89a37842a8e5f4dc92ae3ed54517b7ad413231fe
Milan Adhikari <adhikarimilan4321@gmail.com>
2026-06-22 19:30:56 +0200

    chore(jamesbond): keep PR scope to game implementation

--- src/jaxatari/core.py
+++ src/jaxatari/core.py
@@ -49,7 +49,6 @@ GAME_MODULES = {
     # "hangman": "jaxatari.games.jax_hangman",
     "hauntedhouse": "jaxatari.games.jax_hauntedhouse",
     "humancannonball": "jaxatari.games.jax_humancannonball",
-    "jamesbond": "jaxatari.games.jax_jamesbond",
     "kangaroo": "jaxatari.games.jax_kangaroo",
     "kingkong": "jaxatari.games.jax_kingkong",
     # "klax": "jaxatari.games.jax_klax",
